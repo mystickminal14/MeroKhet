7466efffe915aea02744f0d0a2778a878ca4d833
Pratik Bomzan <121595947+hckkakashi@users.noreply.github.com>
2024-12-02 19:24:31 +0545

    created item view page

---
+++ lib/dashboard/itemview.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Itemview extends StatefulWidget {
+  const Itemview({super.key});
+
+  @override
+  State<Itemview> createState() => _ItemviewState();
+}
+
+class _ItemviewState extends State<Itemview> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
